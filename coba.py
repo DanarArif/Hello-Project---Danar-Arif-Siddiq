15ce7ab23422f3d6ef36b26db1fe862b22fc6b89
DanarArif <Danararif33@gmail.com>
2022-03-18 10:12:21 +0700

    Create coba.py

---
+++ coba.py
@@ -0,0 +1,64 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello world
+      
+SyntaxError: EOL while scanning string literal
+>>> print(1+1)
+2
+>>> print("Wilujeng Sumping")
+Wilujeng Sumping
+>>> print("Hello", "World!", sep=' ')
+Hello World!
+>>> print("Hello", "World!", sep='2')
+Hello2World!
+>>> print("Hello", "World!", sep='2' ,end='3')
+Hello2World!3
+>>> print("Hello' ,"World", sep='good')
+      
+SyntaxError: invalid syntax
+>>> print("Hello","World", sep='good')
+HellogoodWorld
+>>> print(6 // 3)
+2
+>>> pirnt (6// 3.)
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    pirnt (6// 3.)
+NameError: name 'pirnt' is not defined
+>>> print (6 // 3.)
+2.0
+>>> print(15%5)
+0
+>>> print(13%5)
+3
+>>> harga = 100
+>>> total_harga
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    total_harga
+NameError: name 'total_harga' is not defined
+>>> print(harga * 2)
+200
+>>> Nama = Danar
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    Nama = Danar
+NameError: name 'Danar' is not defined
+>>> nama=danar
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    nama=danar
+NameError: name 'danar' is not defined
+>>> nama="Danar"
+>>> print(nama)
+Danar
+>>> nama_lengkap="Danar Arif Siddiq"
+>>> print(nama_lengkap)
+Danar Arif Siddiq
+>>> print(nama)
+Danar
+>>> print(2**2**2)
+16
+>>> print(20//3)
+6
+>>> 
